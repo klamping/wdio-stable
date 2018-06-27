79af653f246e98e3ace9a72be5cac282cb90da16
Kevin Lamping <klamping@gmail.com>
2018-06-27 15:50:50 -0500

    update test

--- test/specs/rightClick.js
+++ test/specs/rightClick.js
@@ -1,6 +1,6 @@
 describe('right click', function () {
   it('should work', function () {
-    browser.url('./');
+    browser.url('./api.html');
     let title = browser.getTitle();
 
     console.log("rightClick.js :6", title);
